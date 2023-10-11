904cdcaa7f3962867eaa33090ce57684baf62401
ki3ani <kimanim68@gmail.com>
2023-10-11 23:38:09 +0300

    fix

--- Lab3-Submission-DataImputation.R
+++ Lab3-Submission-DataImputation.R
@@ -1,144 +1,60 @@
-lapply(.libPaths(), list.files)
+#STEP 1.  SUBSET CREATION
 
-if (!is.element("languageserver", installed.packages()[, 1])) {
-  install.packages("languageserver", dependencies = TRUE,
-                   repos = "https://cloud.r-project.org")
-}
-require("languageserver")
+StudentPerformanceDataset <- read.csv("/home/ki3ani/BBT4206-R-Lab3of15-DataImputation-lumin/markdown/performance-dataset.csv")
 
-# Introduction ----
-# Data imputation, also known as missing data imputation, is a technique used
-# in data analysis and statistics to fill in missing values in a dataset.
-# Missing data can occur due to various reasons, such as equipment malfunction,
-# human error, or non-response in surveys.
 
-# Imputing missing data is important because many statistical analysis methods
-# and Machine Learning algorithms require complete datasets to produce accurate
-# and reliable results. By filling in the missing values, data imputation helps
-# to preserve the integrity and usefulness of the dataset.
+# Load the necessary library
+# Load the necessary library
+library(dplyr)
 
-## dplyr ----
-if (!is.element("dplyr", installed.packages()[, 1])) {
-  install.packages("dplyr", dependencies = TRUE,
-                   repos = "https://cloud.r-project.org")
-}
-require("dplyr")
+# View the column names of your dataset
+colnames(StudentPerformanceDataset)
 
-## naniar ----
-# Documentation:
-#   https://cran.r-project.org/package=naniar or
-#   https://www.rdocumentation.org/packages/naniar/versions/1.0.0
-if (!is.element("naniar", installed.packages()[, 1])) {
-  install.packages("naniar", dependencies = TRUE,
-                   repos = "https://cloud.r-project.org")
-}
-require("naniar")
+# Create a subset of the StudentPerformanceDataset
+StudentPerformanceSubset <- StudentPerformanceDataset %>%
+  select(`A...1..I.am.enjoying.the.subject`,
+         `A...2..Classes.start.and.end.on.time`,
+         `A...4..The.subject.content.is.delivered.according.to.the.course.outline.and.meets.my.expectations`,
+         `TOTAL...Coursework.TOTAL...EXAM..100..`,
+         `EXAM..x.60..60..`,
+         `Absenteeism.Percentage`,
+         `CAT.2..8....x.100.x.100`,
+         `CAT.1..8....x.38.x.100`,
+         `Lab.Work..7...x.25.x.100`,
+         `Lab.4...2.h.....Linear.Discriminant.Analysis..x.5`)
 
-## ggplot2 ----
-# We require the "ggplot2" package to create more appealing visualizations
-if (!is.element("ggplot2", installed.packages()[, 1])) {
-  install.packages("ggplot2", dependencies = TRUE,
-                   repos = "https://cloud.r-project.org")
-}
-require("ggplot2")
 
-## MICE ----
-# We use the MICE package to perform data imputation
-if (!is.element("mice", installed.packages()[, 1])) {
-  install.packages("mice", dependencies = TRUE,
-                   repos = "https://cloud.r-project.org")
-}
-require("mice")
+##data_type2 <- typeof('Absenteeism Percentage')
+##print(data_type2)
 
-## Amelia ----
-if (!is.element("Amelia", installed.packages()[, 1])) {
-  install.packages("Amelia", dependencies = TRUE,
-                   repos = "https://cloud.r-project.org")
-}
-require("Amelia")
+## Now we select 300 observations that would then be included in the dataset
 
-## readr ----
-if (require("readr")) {
-  require("readr")
-} else {
-  install.packages("readr", dependencies = TRUE,
-                   repos = "https://cloud.r-project.org")
-}
+n <- nrow(StudentPerformanceDataset)
+rand_ind <- sample(seq_len(n), 30)
+StudentPerformanceSubset <- StudentPerformanceDataset[rand_ind, ]
 
-# STEP 3. Load the Dataset ----
-student_performance_dataset <-
-  read_csv("/home/ki3ani/BBT4206-R-Lab3of15-DataImputation-lumin/markdown/performance-dataset.csv",
-           col_types =
-             cols(
-               class_group = col_factor(levels = c("A", "B", "C")),
-               gender = col_factor(levels = c("1", "0")),
-               YOB = col_date(format = "%Y"),
-               regret_choosing_bi = col_factor(levels = c("1", "0")),
-               drop_bi_now = col_factor(levels = c("1", "0")),
-               motivator = col_factor(levels = c("1", "0")),
-               read_content_before_lecture =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               anticipate_test_questions =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               answer_rhetorical_questions =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               find_terms_I_do_not_know =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               copy_new_terms_in_reading_notebook =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               take_quizzes_and_use_results =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               reorganise_course_outline =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               write_down_important_points =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               space_out_revision =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               studying_in_study_group =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               schedule_appointments =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               goal_oriented = col_factor(levels = c("1", "0")),
-               spaced_repetition =
-                 col_factor(levels = c("1", "2", "3", "4")),
-               testing_and_active_recall =
-                 col_factor(levels = c("1", "2", "3", "4")),
-               interleaving = col_factor(levels = c("1", "2", "3", "4")),
-               categorizing = col_factor(levels = c("1", "2", "3", "4")),
-               retrospective_timetable =
-                 col_factor(levels = c("1", "2", "3", "4")),
-               cornell_notes = col_factor(levels = c("1", "2", "3", "4")),
-               sq3r = col_factor(levels = c("1", "2", "3", "4")),
-               commute = col_factor(levels = c("1", "2", "3", "4")),
-               study_time = col_factor(levels = c("1", "2", "3", "4")),
-               repeats_since_Y1 = col_integer(),
-               paid_tuition = col_factor(levels = c("0", "1")),
-               free_tuition = col_factor(levels = c("0", "1")),
-               extra_curricular = col_factor(levels = c("0", "1")),
-               sports_extra_curricular = col_factor(levels = c("0", "1")),
-               exercise_per_week = col_factor(levels = c("0", "1", "2", "3")),
-               meditate = col_factor(levels = c("0", "1", "2", "3")),
-               pray = col_factor(levels = c("0", "1", "2", "3")),
-               internet = col_factor(levels = c("0", "1")),
-               laptop = col_factor(levels = c("0", "1")),
-               family_relationships =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               friendships = col_factor(levels = c("1", "2", "3", "4", "5")),
-               romantic_relationships =
-                 col_factor(levels = c("0", "1", "2", "3", "4")),
-               spiritual_wellnes =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               financial_wellness =
-                 col_factor(levels = c("1", "2", "3", "4", "5")),
-               health = col_factor(levels = c("1", "2", "3", "4", "5")),
-               day_out = col_factor(levels = c("0", "1", "2", "3")),
-               night_out = col_factor(levels = c("0", "1", "2", "3")),
-               alcohol_or_narcotics =
-                 col_factor(levels = c("0", "1", "2", "3")),
-               mentor = col_factor(levels = c("0", "1")),
-               mentor_meetings = col_factor(levels = c("0", "1", "2", "3")),
-               `Attendance Waiver Granted: 1 = Yes, 0 = No` =
-                 col_factor(levels = c("0", "1")),
-               GRADE = col_factor(levels = c("A", "B", "C", "D", "E"))),
-           locale = locale())
+# STEP 2. Check the missingness before Imputation
 
+any_na(StudentPerformanceSubset)
+
+# How many?
+n_miss(StudentPerformanceSubset)
+
+# What is the percentage of missing data in the entire dataset?
+prop_miss(StudentPerformanceSubset)
+
+# How many missing values does each variable have?
+StudentPerformanceSubset %>% is.na() %>% colSums()
+
+# What is the number and percentage of missing values grouped by
+# each variable?
+miss_var_summary(StudentPerformanceSubset)
+
+# What is the number and percentage of missing values grouped by
+# each observation?
+miss_case_summary(StudentPerformanceSubset)
+
+# Which variables contain the most missing values?
+gg_miss_var(StudentPerformanceSubset)
+
+# Where are missing values located (the shaded re
